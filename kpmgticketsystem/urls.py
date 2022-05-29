9a4cd417fea98fa025d7c605370430ed8587ecb2
houdabssr <gn_boussouar@esi.dz>
2022-05-30 00:07:18 +0200

    ch(deploy): Deploy basic app

--- kpmgticketsystem/urls.py
+++ kpmgticketsystem/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include('tickets.urls')),
+    path('/', include('tickets.urls')),
     path('authentication/',include('authentication.urls')),
 
     path('admin/', admin.site.urls),
